6946727086c58fe19779237ff6e10c7f530354fe
Anil <anilkumar.kotur@go-jek.com>
2018-03-11 14:30:20 +0530

    added earth and rotate action to it

--- 3DWorld/3DWorld/ViewController.swift
+++ 3DWorld/3DWorld/ViewController.swift
@@ -25,15 +25,25 @@ class ViewController: UIViewController {
 
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0,y: 0, z: 5)
         
         scene.rootNode.addChildNode(cameraNode)
         
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = .omni
+        lightNode.position = SCNVector3(x: 0,y: 10, z: 2)
+        
+        scene.rootNode.addChildNode(lightNode)
         let sceneView = self.view as! SCNView
         sceneView.scene = scene
     
         if let stars = SCNParticleSystem(named: "StarParticles.scnp", inDirectory: nil) {
             scene.rootNode.addParticleSystem(stars)
         }
+        
+        let earthNode = EarthNode()
+        scene.rootNode.addChildNode(earthNode)
         //To see statistics
         sceneView.showsStatistics = true
         sceneView.backgroundColor = .black
